e01203eaeb73dad08d9eea5afebff1114b1e2cb1
Jackey <Jackey.Zhuang@thebeaglearmada.nl>
2013-04-17 17:05:24 +0800

    Initial files

---
+++ Scripts/JScript.js
@@ -0,0 +1,68 @@
+﻿$(document).ready(function() {
+    $('.fancybox').fancybox();
+
+    // Change title type, overlay closing speed
+    $(".fancybox-effects-a").fancybox({
+        helpers: {
+            title: {
+                type: 'outside'
+            },
+            overlay: {
+                speedOut: 0
+            }
+        }
+    });
+
+    // Disable opening and closing animations, change title type
+    $(".fancybox-effects-b").fancybox({
+        openEffect: 'none',
+        closeEffect: 'none',
+
+        helpers: {
+            title: {
+                type: 'over'
+            }
+        }
+    });
+
+    // Set custom style, close if clicked, change title type and overlay color
+    $(".fancybox-effects-c").fancybox({
+        wrapCSS: 'fancybox-custom',
+        closeClick: false,
+
+        openEffect: 'none',
+
+        helpers: {
+            title: {
+                type: 'inside'
+            },
+            overlay: {
+                css: {
+                    'background': 'rgba(238,238,238,0.85)'
+                }
+            }
+        }
+    });
+
+    // Remove padding, set opening and closing animations, close if clicked and disable overlay
+    $(".fancybox-effects-d").fancybox({
+        padding: 0,
+
+        openEffect: 'elastic',
+        openSpeed: 150,
+
+        closeEffect: 'elastic',
+        closeSpeed: 150,
+
+        closeClick: true,
+
+        helpers: {
+            overlay: {
+                css: {
+                    'background': 'rgba(238,238,238,0.85)'
+                }
+            }
+        }
+    });
+
+});
